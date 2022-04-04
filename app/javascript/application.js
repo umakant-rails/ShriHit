b34f9fbf4f0f1f6126de2295d22423a605a22d55
umakant-rails <rajputumakant@gmail.com>
2022-04-04 14:49:09 +0530

    add bootstrap 5 and update layout

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,3 @@
 import 'jquery';
-import "popper"
-import "bootstrap"
+import "popper";
+import "bootstrap";
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,6 +1,7 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application", preload: true
-pin "jquery", to: "https://ga.jspm.io/npm:jquery@3.6.0/dist/jquery.js"
+pin "jquery", to: "https://ga.jspm.io/npm:jquery@3.6.0/dist/jquery.js", preload: true
 pin "popper", to: 'popper.js', preload: true
 pin "bootstrap", to: 'bootstrap.min.js', preload: true
+pin "layout", to: "layout.js", preload: true
