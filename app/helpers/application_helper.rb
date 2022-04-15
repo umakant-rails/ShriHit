24a3f5b4b10095e8746a50334924b28c35b01c26
umakant-rails <rajputumakant@gmail.com>
2022-04-15 16:32:45 +0530

    add active class to selected feature in sidebar menu

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def set_active(path_params, path_string)
+    if (path_params == path_string)
+      return "active"
+    elsif !path_params.index('new') && path_params.index(path_string) != nil
+      return "active"
+    end
+  end
+
 end
