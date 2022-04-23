1188ccab82b57417ce4ced051d4396d2c315088b
umakant-rails <rajputumakant@gmail.com>
2022-04-23 21:25:24 +0530

    add author list at welcome index page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,12 +1,14 @@
 class WelcomeController < ApplicationController
 
   def index
-    @articles = Article.all.order("created_at DESC")
+    @articles = Article.all.order("created_at DESC").limit(10)
+    @authors = Author.all.order("created_at DESC").limit(10)  
   end
 
   def autocomplete_term
     search_term = params[:q]
-    @articles = Article.where("LOWER(english_title) like ?", "%#{search_term.downcase}%")
+    @articles = Article.where("LOWER(english_title) like ? or LOWER(hindi_title) like ?",
+      "%#{search_term.downcase}%", "%#{search_term.downcase}%")
     respond_to do |format|
       format.html {}
       format.json { head :no_content }
@@ -16,7 +18,11 @@ class WelcomeController < ApplicationController
 
   def search_term
     search_term = params[:search][:term]
-    @articles = Article.where("LOWER(english_title) like ?", "%#{search_term.downcase}%")
+    indexx = search_term.index(":")
+    search_term = indexx.nil? ? search_term.strip : search_term[0, indexx].strip
+    
+    @articles = Article.where("LOWER(english_title) like ? or LOWER(hindi_title) like ?",
+      "%#{search_term.downcase}%", "%#{search_term.downcase}%")
 
     respond_to do |format|
       format.html {}
--- config/importmap.rb
+++ config/importmap.rb
@@ -5,10 +5,11 @@ pin "jquery", to: "https://ga.jspm.io/npm:jquery@3.6.0/dist/jquery.js", preload:
 pin "popper", to: 'popper.js', preload: true
 pin "bootstrap", to: 'bootstrap.min.js', preload: true
 pin "layout", to: "layout.js", preload: true
-#pin "transliteration", to: "transliteration.I.js", preload: true
-#pin "summernote", to: "https://cdn.jsdelivr.net/npm/summernote@0.8.20/dist/summernote-lite.min.js", preload: true
-pin "tinymce", to: "https://cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js", preload: true
+# pin "transliteration", to: "transliteration.I.js", preload: true
+# pin "summernote", to: "https://cdn.jsdelivr.net/npm/summernote@0.8.20/dist/summernote-lite.min.js", preload: true
+# pin "tinymce", to: "https://cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js", preload: true
 
+pin "tinymce", to: "tinymce.min.js", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 #pin "@hotwired/stimulus", to: "https://ga.jspm.io/npm:@hotwired/stimulus@3.0.1/dist/stimulus.js"
@@ -19,4 +20,5 @@ pin "stimulus", to: "https://ga.jspm.io/npm:stimulus@1.1.1/dist/stimulus.umd.js"
 pin "validate.js", to: "https://ga.jspm.io/npm:validate.js@0.12.0/validate.js"
 pin "stimulus-autocomplete", to: "https://ga.jspm.io/npm:stimulus-autocomplete@3.0.2/src/autocomplete.js"
 
-pin "article-custom"
+pin "article-custom";
+
