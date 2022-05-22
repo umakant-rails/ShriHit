1702334c086907d1ea1699f68b2cfc8f9e9fc844
umakant-rails <rajputumakant@gmail.com>
2022-05-22 11:39:33 +0530

    user home page html

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,6 +2,8 @@ class HomesController < ApplicationController
 	before_action :authenticate_user!
 
 	def index
+		@articles = current_user.articles
+		@authors = current_user.authors
 	end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_07_104727) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_22_042835) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -43,6 +43,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_07_104727) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "is_saint", default: false
   end
 
   create_table "contexts", force: :cascade do |t|
