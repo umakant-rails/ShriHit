b82770cb6cfe7e894573bcaa7f7f80fa227569b0
umakant-rails <rajpootumakant@gmail.com>
2023-01-27 15:35:09 +0530

    change text on author pages html

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -3,12 +3,13 @@ class Tag < ApplicationRecord
   has_many :article_tags
   has_many :articles, through: :article_tags
   belongs_to :user
-
+  validates :name, presence: true
+  # validates :cep, presence: { message: 'Your custom error message' }, unless: lambda { |o| o.location }
   paginates_per 10
 
   scope :pending, ->() { where(is_approved: false) }
   scope :approved, ->() { where(is_approved: true) }
-  
+
   def self.create_tags(current_user, param_tags)
     param_tags.each_with_index{| tag, index | param_tags[index] = tag.strip }
 
