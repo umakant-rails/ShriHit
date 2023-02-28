3c1b97a3a210bad2cf835f3099ff91eeeabbda41
umakant-rails <rajpootumakant@gmail.com>
2023-02-28 23:43:21 +0530

    fix issue for deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :repo_url, "git@github.com:umakant-rails/ShriHit.git"
 ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, "/apps/shrihit"
+set :deploy_to, "/home/deploy/apps/shrihit"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
