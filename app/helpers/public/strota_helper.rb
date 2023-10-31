4ff4fc85b33a22c54222e8c1be102b98ed6f7571
umakant-rails <rajpootumakant@gmail.com>
2023-10-31 17:54:34 +0530

    update html

--- app/helpers/public/strota_helper.rb
+++ app/helpers/public/strota_helper.rb
@@ -1,12 +1,12 @@
 module Public::StrotaHelper
   
-  def get_strota_content(content, indx)
+  def get_strota_content(content, indx=nil)
     indexing1 = "॥#{(indx+1).to_s}॥"
     indexing2 = "।।#{(indx+1).to_s}।।"
 
-    if content.index(indexing1).blank? and content.index("॥").present?
+    if indx.present? && content.index(indexing1).blank? and content.index("॥").present?
       content.gsub("॥", indexing1)
-    elsif content.index(indexing2).blank? and content.index("।।").present?
+    elsif indx.present? && content.index(indexing2).blank? and content.index("।।").present?
       content.gsub("।।", indexing2)
     else 
       content
