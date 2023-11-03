440ccb9a2b9818be215622ab2b0e61262905d069
umakant-rails <rajpootumakant@gmail.com>
2023-11-03 17:41:51 +0530

    update scripture show page

--- app/javascript/controllers/scripture_controller.js
+++ app/javascript/controllers/scripture_controller.js
@@ -19,30 +19,14 @@ export default class extends ApplicationController {
       newTarget.classList.remove('hide');
     }
   }
-  next(event){
-    // var pageIndex = event.target.dataset.page;
-    // pageIndex = parseInt(pageIndex) + 1;
-    // var target = this.pageTargets.find( target => target.dataset.page == pageIndex);
-    // if (target != undefined) {
-    //   this.displayPageData(target, pageIndex);
-    // } else {
-    //   this.nextTarget.disabled = true;
-    // }
+  navigationNext(){
     var target = this.pageTargets.find( target => !target.classList.contains('hide'));
     var pageIndex = parseInt(target.dataset.page) + 1;
 
     this.displayPageData(target, pageIndex);
   }
 
-  previous(event){
-    // var pageIndex = event.target.dataset.page;
-    // pageIndex = parseInt(pageIndex) - 1;
-    // var target = this.pageTargets.find( target => target.dataset.page == pageIndex);
-    // if (target != undefined) {
-    //   this.displayPageData(target, pageIndex);
-    // } else {
-    //   this.previousTarget.disabled = true;
-    // }
+  navigationPrevious(){
     var target = this.pageTargets.find( target => !target.classList.contains('hide'));
     var pageIndex = parseInt(target.dataset.page) - 1;
     this.displayPageData(target, pageIndex);
@@ -52,18 +36,35 @@ export default class extends ApplicationController {
     var target = this.pageTargets.find( target => !target.classList.contains('hide'));
     var pageIndex = parseInt(event.target.dataset.index);
     this.pageParentTarget.classList.remove('hide');
-    this.indexingPageTarget.classList.add('hide');
+    // this.indexingPageTarget.classList.add('hide');
     this.displayPageData(target, pageIndex);
   }
 
   displayPageIndexing(event){
     var isHideIndexingPage = this.indexingPageTarget.classList.contains('hide');
     if(isHideIndexingPage){
-      this.pageParentTarget.classList.add('hide');
+      // this.pageParentTarget.classList.add('hide');
       this.indexingPageTarget.classList.remove('hide');
+      this.pageParentTarget.classList.remove('col-md-12');
+      this.pageParentTarget.classList.add('col-md-8');
     } else {
-      this.pageParentTarget.classList.remove('hide');
+      // this.pageParentTarget.classList.remove('hide');
       this.indexingPageTarget.classList.add('hide');
+      this.pageParentTarget.classList.remove('col-md-8');
+      this.pageParentTarget.classList.add('col-md-12');
+    }
+  }
+
+  navigation(event){
+    var mousePosition = event.pageX;
+    var offset = this.pageParentTarget.getBoundingClientRect();
+    var divLeft = Math.round(offset.left);
+    var divRight = Math.round(offset.right);
+    var divHalfPosition = divLeft + (divRight-divLeft)/2;
+    if(mousePosition < divHalfPosition){
+      this.navigationPrevious();
+    } else {
+      this.navigationNext()
     }
   }
 }
