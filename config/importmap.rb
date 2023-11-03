232bc742b56f0bef0a0d5227c67938d9002d4217
umakant-rails <rajpootumakant@gmail.com>
2023-11-03 23:13:42 +0530

    update importmap file

--- config/importmap.rb
+++ config/importmap.rb
@@ -24,10 +24,9 @@ pin "flatpickr", to: "https://ga.jspm.io/npm:flatpickr@4.6.13/dist/esm/index.js"
 pin "chartkick", to: "chartkick.js"
 pin "Chart.bundle", to: "Chart.bundle.js"
 pin "swiper", to: "https://cdn.jsdelivr.net/npm/swiper@9/swiper-element-bundle.min.js"
+pin "@rails/ujs", to: "https://ga.jspm.io/npm:@rails/ujs@7.0.3-1/lib/assets/compiled/rails-ujs.js"
 
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin_all_from "app/javascript/custom", under: "custom"
 # pin "article-custom";
 # pin "user-profile-custom";
-
-pin "@rails/ujs", to: "https://ga.jspm.io/npm:@rails/ujs@7.0.3-1/lib/assets/compiled/rails-ujs.js"
